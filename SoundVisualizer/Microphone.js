9aa38cd7b3181a72cd68c3ac8f17530a4c995b55
Corey <Corey255A1@gmail.com>
2019-11-07 08:18:38 -0500

    Updates to the Sound Viz

--- SoundVisualizer/Microphone.js
+++ SoundVisualizer/Microphone.js
@@ -1,17 +1,31 @@
-﻿const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+function AudioViz(){
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 const canvas = document.getElementById("viz");
 const drawCtx = canvas.getContext("2d");
 
+const plotcanvas = document.getElementById("plot");
+const plotdrawCtx = plotcanvas.getContext("2d");
+
+var WIDTH = canvas.width;
+var HEIGHT = canvas.height;
+
+var PLOTWIDTH = plotcanvas.width;
+var PLOTHEIGHT = plotcanvas.height
+
 const audioBuff = audioCtx.createBuffer(1, 5000, 10000);
 const audioAnalyser = audioCtx.createAnalyser();
-audioAnalyser.fftSize = 1024;
+audioAnalyser.fftSize = 256;
 audioAnalyser.minDecibels = -90;
 audioAnalyser.maxDecibels = -10;
 
 const buffSize = audioAnalyser.frequencyBinCount;
-const dataBuff = new Float32Array(buffSize);
+//const dataBuff = new Float32Array(buffSize);
+const dataBuff = new Uint8Array(buffSize);
 
+const barWidth = WIDTH/buffSize;
 var mic;
+
+var timeOffset = 0;
 
 navigator.mediaDevices.getUserMedia({ audio: true })
     .then(function (s) {
@@ -29,22 +43,40 @@ navigator.mediaDevices.getUserMedia({ audio: true })
 
 function update()
 {
-    audioAnalyser.getFloatFrequencyData(dataBuff);
+    audioAnalyser.getByteFrequencyData(dataBuff);
     draw(dataBuff);
     window.requestAnimationFrame(update)
 }
 function draw(buff)
 {
-    drawCtx.clearRect(0, 0, 512, 128);
+    drawCtx.clearRect(0, 0, WIDTH, HEIGHT);
     drawCtx.fillStyle = 'rgb(0,0,0)';
-    drawCtx.fillRect(0, 0, 512, 128);
+    drawCtx.fillRect(0, 0, WIDTH, HEIGHT);
     drawCtx.fillStyle = 'rgb(255,0,0)';
-    var offset = 0;
-    for (var d in buff)
-    {
-        var s = 200+buff[d];
-        drawCtx.fillRect(offset, 128-(s), 1, (s));
-        
-        offset = offset + 1;
+    
+    if(timeOffset==0){
+        plotdrawCtx.clearRect(0, 0, PLOTWIDTH, PLOTHEIGHT);
+        plotdrawCtx.fillStyle = 'rgb(0,0,0)';
+        plotdrawCtx.fillRect(0, 0, PLOTWIDTH, PLOTHEIGHT);
     }
+    
+    var offset = 0;
+    for (var d=0; d<buffSize; d++)
+    {
+        var s = buff[d];
+        drawCtx.fillRect(offset, HEIGHT-s/2, barWidth, s);
+        plotdrawCtx.fillStyle = 'rgb(0,0,'+s+')';
+        plotdrawCtx.fillRect(offset, timeOffset, barWidth, barWidth);
+        
+        offset = offset + barWidth;
+    }
+    timeOffset = (timeOffset+1)%PLOTHEIGHT;
 }
+
+}
+
+const enable = document.querySelector("#enable");
+enable.addEventListener("click",()=>{
+    AudioViz();
+})
+
